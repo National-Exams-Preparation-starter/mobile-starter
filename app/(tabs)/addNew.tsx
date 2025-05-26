e0280ca25adad53fb6e712af946ee9f73cbd2603
LuckyBelieve <luckybelieve10@gmail.com>
2025-05-26 15:17:03 +0200

    feat:progress add page

--- app/(tabs)/addNew.tsx
+++ app/(tabs)/addNew.tsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AppLayout from '@/components/common/AppLayout';
+import AppHeader from '@/components/common/AppHeader';
+import AddContainer from '@/components/common/AddContainer';
 
 const addNew = () => {
   return (
-    <View>
-      <Text>addNew</Text>
-    </View>
+    <AppLayout>
+      <View className='flex-row items-center justify-center py-5'>
+        <Text className='text-lg font-senSemiBold font-semibold text-primary'>Add</Text>
+      </View>
+      <View style={{ flex: 1 }}> 
+        <AddContainer/>
+      </View>
+    </AppLayout>
   )
 }
 
